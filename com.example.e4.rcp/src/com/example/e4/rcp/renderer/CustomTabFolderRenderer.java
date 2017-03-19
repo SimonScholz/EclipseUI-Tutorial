6b033023ddb9a5c58a5227f39f597a3bdff452bc
Simon Scholz <simon.scholz@vogella.com>
2017-03-19 23:11:00 +0100

    Added inline documentation
    Change-Id: Ia32fff106b02f5978bf66039d0b624c356cc360b
    Signed-off-by: Simon Scholz <simon.scholz@vogella.com>

--- com.example.e4.rcp/src/com/example/e4/rcp/renderer/CustomTabFolderRenderer.java
+++ com.example.e4.rcp/src/com/example/e4/rcp/renderer/CustomTabFolderRenderer.java
@@ -14,29 +14,39 @@ import org.eclipse.swt.graphics.Rectangle;
 
 public class CustomTabFolderRenderer extends CTabFolderRenderer {
 
+	private static final int TAB_WIDTH_ADJUSTMENT = 20;
+	private static final int TAB_HEIGHT = 50;
 	private static final int UNDERLINE_TRIM_HEIGHT = 5;
 	private ResourceManager resourceManager;
 
 	public CustomTabFolderRenderer(CTabFolder parent) {
 		super(parent);
+		// resource manager for proper resource handling
 		resourceManager = new LocalResourceManager(JFaceResources.getResources(), parent);
 	}
 
 	@Override
 	protected Point computeSize(int part, int state, GC gc, int wHint, int hHint) {
+		// get the sizes for other parts of the CTabFolder
 		Point computeSize = super.computeSize(part, state, gc, wHint, hHint);
 
+		// handle the size of the tabs
 		if (0 <= part && part < parent.getItemCount()) {
-			computeSize.x += 20;
+			// make parts bigger
+			computeSize.x += TAB_WIDTH_ADJUSTMENT;
+			// return the computed x size and a fixed tab height
+			return new Point(computeSize.x, TAB_HEIGHT);
 		}
 
-		return new Point(computeSize.x, 50);
+		return computeSize;
 	}
 
 	@Override
 	protected void draw(int part, int state, Rectangle bounds, GC gc) {
+		// default drawing is done in the super class
 		super.draw(part, state, bounds, gc);
 
+		// draw on the selected tab
 		adjustSelectedTab(part, state, bounds, gc);
 	}
 
@@ -54,8 +64,9 @@ public class CustomTabFolderRenderer extends CTabFolderRenderer {
 		Color origBg = gc.getBackground();
 		Color bgColor = resourceManager.createColor(new RGB(0, 132, 180));
 		gc.setBackground(bgColor);
-		gc.fillRectangle(bounds.x, bounds.y + bounds.height - UNDERLINE_TRIM_HEIGHT, bounds.width, UNDERLINE_TRIM_HEIGHT);
+		// draw line at the bottom
+		gc.fillRectangle(bounds.x, bounds.y + bounds.height - UNDERLINE_TRIM_HEIGHT, bounds.width,
+				UNDERLINE_TRIM_HEIGHT);
 		gc.setBackground(origBg);
 	}
-
 }
